2446dd75e8d077a1f36bf3273b83f68af2b1a62c
Martin Albert <martin.albert.187@gmail.com>
2024-01-15 14:34:23 +0100

    feat: prepare CLI - use logger from context instead of `fmt` - prepare credentials in config for workspace API - refactor the flow of config validation   - account id has be present at all times   - currently only Basic or SCIM are supported - comment out handling workspace API configuration for now

--- cmd/baton-databricks/config.go
+++ cmd/baton-databricks/config.go
@@ -12,54 +12,50 @@ import (
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
 
-	// Account API requires account id AND SCIM token OR username and password
-	AccountId string `mapstructure:"account-id"`
-
-	// Workspace API requires at least one workspace AND access token OR username and password
+	AccountId  string   `mapstructure:"account-id"`
+	SCIMToken  string   `mapstructure:"scim-token"`
+	Username   string   `mapstructure:"username"`
+	Password   string   `mapstructure:"password"`
 	Workspaces []string `mapstructure:"workspaces"`
-
-	SCIMToken string `mapstructure:"scim-token"`
-	Token     string `mapstructure:"token"`
-	Username  string `mapstructure:"username"`
-	Password  string `mapstructure:"password"`
+	Tokens     []string `mapstructure:"workspace-tokens"`
 }
 
 func (c *config) IsBasicAuth() bool {
 	return c.Username != "" && c.Password != ""
 }
 
-func (c *config) IsTokenAuth() bool {
-	return c.Token != ""
-}
-
 func (c *config) IsSCIMAuth() bool {
 	return c.SCIMToken != ""
 }
 
-func (c *config) IsAccountAPI() bool {
-	return c.AccountId != ""
+func (c *config) AreWorkspacesSet() bool {
+	return len(c.Workspaces) > 0
 }
 
-func (c *config) IsWorkspaceAPI() bool {
-	return len(c.Workspaces) > 0
+func (c *config) AreTokensSet() bool {
+	return len(c.Workspaces) == len(c.Tokens)
+}
+
+func (c *config) IsAccAuthReady() bool {
+	return c.IsSCIMAuth() || c.IsBasicAuth()
+}
+
+func (c *config) IsWorkspaceAuthReady() bool {
+	return (c.AreWorkspacesSet() && c.AreTokensSet()) || c.IsBasicAuth()
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
-	if !cfg.IsAccountAPI() && !cfg.IsWorkspaceAPI() {
-		return fmt.Errorf("either account ID and SCIM token or access token must be provided, use --help for more information")
+	if cfg.AccountId == "" {
+		return fmt.Errorf("account ID must be provided, use --help for more information")
 	}
 
-	if cfg.IsAccountAPI() {
-		if !cfg.IsSCIMAuth() && !cfg.IsBasicAuth() {
-			return fmt.Errorf("SCIM token or username and password must be provided, use --help for more information")
-		}
+	if !cfg.IsAccAuthReady() {
+		return fmt.Errorf("either SCIM token or username and password must be provided, use --help for more information")
 	}
 
-	if cfg.IsWorkspaceAPI() {
-		if !cfg.IsTokenAuth() && !cfg.IsBasicAuth() {
-			return fmt.Errorf("access token or username and password must be provided, use --help for more information")
-		}
+	if !cfg.IsWorkspaceAuthReady() {
+		return fmt.Errorf("either access token along workspaces or username and password must be provided, use --help for more information")
 	}
 
 	return nil
@@ -68,11 +64,9 @@ func validateConfig(ctx context.Context, cfg *config) error {
 // cmdFlags sets the cmdFlags required for the connector.
 func cmdFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().String("scim-token", "", "The Databricks SCIM token used to connect to the Databricks Account API. ($BATON_SCIM_TOKEN)")
-	cmd.PersistentFlags().String("account-id", "", "The Databricks account ID used to connect to the Databricks Account API. ($BATON_ACCOUNT_ID)")
-	cmd.PersistentFlags().String("token", "", "The Databricks access token used to connect to the Databricks Workspace API. ($BATON_TOKEN)")
-	cmd.PersistentFlags().StringSlice("workspaces", []string{}, "Limit syncing to the specified workspaces. ($BATON_WORKSPACES)")
+	cmd.PersistentFlags().String("account-id", "", "The Databricks account ID used to connect to the Databricks Account and Workspace API. ($BATON_ACCOUNT_ID)")
 	cmd.PersistentFlags().String("username", "", "The Databricks username used to connect to the Databricks API. ($BATON_USERNAME)")
 	cmd.PersistentFlags().String("password", "", "The Databricks password used to connect to the Databricks API. ($BATON_PASSWORD)")
-	// TODO: support token per workspace slice:
-	// cmd.PersistentFlags().StringSlice("tokens", []string{}, "The Databricks access tokens used to connect to the Databricks API. ($BATON_TOKENS)")
+	cmd.PersistentFlags().StringSlice("workspaces", []string{}, "Limit syncing to the specified workspaces. ($BATON_WORKSPACES)")
+	cmd.PersistentFlags().StringSlice("workspace-tokens", []string{}, "The Databricks access tokens scoped to specific workspaces used to connect to the Databricks Workspace API. ($BATON_WORKSPACE_TOKENS)")
 }
--- cmd/baton-databricks/main.go
+++ cmd/baton-databricks/main.go
@@ -38,72 +38,55 @@ func main() {
 }
 
 // config defines the external configuration required for the connector to run.
-// you can run this connector in multiple modes:
-// 1. Account API mode: you provide a SCIM token and an account ID, and the connector will use the Account API to fetch resources.
-// 2. Workspace API mode: you provide an access token and a list of workspaces, and the connector will use the Workspace API to fetch resources.
-// TODO 3. Both?
-func prepareClientConfigs(cfg *config) []databricks.Config {
+// you can run this connector in multiple modes, Account API mode or Workspace API mode
+// if you specify workspaces, connector will try to sync resources in those workspaces as well
+func prepareClientConfigs(ctx context.Context, cfg *config) []databricks.Config {
+	var cv []databricks.Config
+	l := ctxzap.Extract(ctx)
+
 	if cfg.IsSCIMAuth() {
 		// SCIM token is only available to use with the Account API
-		fmt.Printf("using account API mode\n\taccount ID: %s\n\tSCIM token: %s\n", cfg.AccountId, cfg.SCIMToken)
+		l.Info("using account API mode", zap.String("account-id", cfg.AccountId), zap.String("scim-token", cfg.SCIMToken))
 
 		cAuth := databricks.NewTokenAuth(cfg.SCIMToken)
-		fmt.Printf("auth: %+v\n", cAuth)
-
 		c := databricks.NewAccountConfig(cfg.AccountId, cAuth)
-		fmt.Printf("config: %+v\n", c)
-
-		return []databricks.Config{c}
-	}
-
-	if cfg.IsTokenAuth() {
-		fmt.Printf("using workspace API mode\n\taccess token: %s\n\tworkspaces: %v\n", cfg.Token, cfg.Workspaces)
-
-		var cv []databricks.Config
-
-		// HERE we're expecting user to have one token over multiple workspaces
-		// TODO: validate if this is possible
-		cAuth := databricks.NewTokenAuth(cfg.Token)
-
-		for _, workspace := range cfg.Workspaces {
-			c := databricks.NewWorkspaceConfig(workspace, cAuth)
-
-			cv = append(cv, c)
-		}
-
-		return cv
-	}
-
-	if cfg.IsBasicAuth() {
-		var cv []databricks.Config
+		cv = append(cv, c)
+	} else if cfg.IsBasicAuth() {
+		l.Info("using account API mode", zap.String("account-id", cfg.AccountId), zap.String("username", cfg.Username))
 		cAuth := databricks.NewBasicAuth(cfg.Username, cfg.Password)
+		c := databricks.NewAccountConfig(cfg.AccountId, cAuth)
+		cv = append(cv, c)
 
-		if cfg.IsAccountAPI() {
-			fmt.Printf("using account API mode\n\taccount ID: %s\n", cfg.AccountId)
-			c := databricks.NewAccountConfig(cfg.AccountId, cAuth)
-			cv = append(cv, c)
-		}
-
-		if cfg.IsWorkspaceAPI() {
+		if cfg.AreWorkspacesSet() {
 			fmt.Printf("using workspace API mode\n\tworkspaces: %v\n", cfg.Workspaces)
 
 			for _, workspace := range cfg.Workspaces {
-				c := databricks.NewWorkspaceConfig(workspace, cAuth)
+				c := databricks.NewWorkspaceConfig(workspace, cfg.AccountId, cAuth)
 				cv = append(cv, c)
 			}
 		}
-
-		return cv
 	}
 
-	return nil
+	return cv
+
+	// TODO: Handle workspaces without basic auth
+	// l.Info("using workspace API mode", zap.String("account-id", cfg.AccountId), zap.Strings("workspaces", cfg.Workspaces))
+	//
+	// if cfg.AreWorkspacesSet() {
+	// 	fmt.Printf("using workspace API mode\n\tworkspaces: %v\n", cfg.Workspaces)
+	// }
+	//
+	// for i, workspace := range cfg.Workspaces {
+	// 	cAuth := databricks.NewTokenAuth(cfg.Tokens[i])
+	// 	c := databricks.NewWorkspaceConfig(workspace, cfg.AccountId, cAuth)
+	//
+	// 	cv = append(cv, c)
 }
 
 func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
-	dc := prepareClientConfigs(cfg)
-
-	cb, err := connector.New(ctx, dc)
+	dc := prepareClientConfigs(ctx, cfg)
+	cb, err := connector.New(ctx, cfg.AccountId, dc)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
