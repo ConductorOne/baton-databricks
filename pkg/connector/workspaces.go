f358152c2e3c511ee5b84625f15b0e85afd95f64
Martin Albert <martin.albert.187@gmail.com>
2024-01-23 22:28:25 +0100

    feat: Implement provisioning for workspace

--- pkg/connector/workspaces.go
+++ pkg/connector/workspaces.go
@@ -11,6 +11,8 @@ import (
 	ent "github.com/conductorone/baton-sdk/pkg/types/entitlement"
 	"github.com/conductorone/baton-sdk/pkg/types/grant"
 	rs "github.com/conductorone/baton-sdk/pkg/types/resource"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -186,6 +188,71 @@ func (w *workspaceBuilder) Grants(ctx context.Context, resource *v2.Resource, pT
 	}
 
 	return rv, "", nil, nil
+}
+
+func (w *workspaceBuilder) Grant(ctx context.Context, principal *v2.Resource, entitlement *v2.Entitlement) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	if !isValidPrincipal(principal.Id) {
+		l.Warn(
+			"databricks-connector: only users, groups and service principals can be granted workspace membership",
+			zap.String("principal_id", principal.Id.String()),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, fmt.Errorf("databricks-connector: only users, groups and service principals can be granted workspace membership")
+	}
+
+	groupTrait, err := rs.GetGroupTrait(entitlement.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to get group trait: %w", err)
+	}
+
+	workspaceID, ok := rs.GetProfileInt64Value(groupTrait.Profile, "workspace_id")
+	if !ok {
+		return nil, fmt.Errorf("databricks-connector: failed to get workspace ID: %w", err)
+	}
+
+	err = w.client.CreateOrUpdateWorkspaceMember(ctx, workspaceID, principal.Id.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to create or update workspace member: %w", err)
+	}
+
+	return nil, nil
+}
+
+func (w *workspaceBuilder) Revoke(ctx context.Context, grant *v2.Grant) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	principal := grant.Principal
+	entitlement := grant.Entitlement
+
+	if !isValidPrincipal(principal.Id) {
+		l.Warn(
+			"databricks-connector: only users, groups and service principals can have workspace membership revoked",
+			zap.String("principal_id", principal.Id.String()),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, fmt.Errorf("databricks-connector: only users, groups and service principals can have workspace membership revoked")
+	}
+
+	groupTrait, err := rs.GetGroupTrait(entitlement.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to get group trait: %w", err)
+	}
+
+	workspaceID, ok := rs.GetProfileInt64Value(groupTrait.Profile, "workspace_id")
+	if !ok {
+		return nil, fmt.Errorf("databricks-connector: failed to get workspace ID: %w", err)
+	}
+
+	err = w.client.RemoveWorkspaceMember(ctx, workspaceID, principal.Id.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to create or update workspace member: %w", err)
+	}
+
+	return nil, nil
 }
 
 func newWorkspaceBuilder(client *databricks.Client, workspaces []string) *workspaceBuilder {
