5618767ab3f2a2de869c3dbde9f534cb16a8b7f8
Martin Albert <martin.albert.187@gmail.com>
2024-01-24 14:40:11 +0100

    feat: Implement provisioning for accounts

--- pkg/connector/account.go
+++ pkg/connector/account.go
@@ -3,6 +3,7 @@ package connector
 import (
 	"context"
 	"fmt"
+	"slices"
 	"strings"
 
 	"github.com/conductorone/baton-databricks/pkg/databricks"
@@ -12,6 +13,8 @@ import (
 	ent "github.com/conductorone/baton-sdk/pkg/types/entitlement"
 	"github.com/conductorone/baton-sdk/pkg/types/grant"
 	rs "github.com/conductorone/baton-sdk/pkg/types/resource"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -90,7 +93,7 @@ func (a *accountBuilder) Entitlements(_ context.Context, resource *v2.Resource, 
 	var rv []*v2.Entitlement
 
 	permissiongOptions := []ent.EntitlementOption{
-		ent.WithGrantableTo(userResourceType, servicePrincipalResourceType),
+		ent.WithGrantableTo(userResourceType, groupResourceType, servicePrincipalResourceType),
 		ent.WithDisplayName(fmt.Sprintf("%s %s role", resource.DisplayName, MarketplaceAdminRole)),
 		ent.WithDescription(fmt.Sprintf("%s %s role in Databricks", resource.DisplayName, MarketplaceAdminRole)),
 	}
@@ -137,6 +140,137 @@ func (a *accountBuilder) Grants(ctx context.Context, resource *v2.Resource, pTok
 	}
 
 	return rv, "", nil, nil
+}
+
+func (a *accountBuilder) Grant(ctx context.Context, principal *v2.Resource, entitlement *v2.Entitlement) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	if !isValidPrincipal(principal.Id) {
+		l.Warn(
+			"databricks-connector: only users, groups and service principals can be granted account permissions",
+			zap.String("principal_id", principal.Id.String()),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, fmt.Errorf("databricks-connector: only users, groups and service principals can be granted account permissions")
+	}
+
+	accID := entitlement.Resource.Id.Resource
+	ruleSets, err := a.client.ListRuleSets(ctx, "", "")
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to list rule sets for account %s: %w", accID, err)
+	}
+
+	principalID, err := preparePrincipalID(ctx, a.client, principal.Id.ResourceType, principal.Id.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to prepare principal id for principal %s: %w", principal.Id.Resource, err)
+	}
+
+	found := false
+
+	for i, ruleSet := range ruleSets {
+		if strings.Contains(ruleSet.Role, entitlement.Slug) {
+			found = true
+
+			// check if it contains the principals and add principal to the rule set
+			if slices.Contains(ruleSet.Principals, principalID) {
+				l.Info(
+					"databricks-connector: account already has the entitlement",
+					zap.String("principal_id", principalID),
+					zap.String("entitlement", entitlement.Slug),
+				)
+
+				return nil, nil
+			}
+
+			// add the principal to the rule set
+			ruleSets[i].Principals = append(ruleSet.Principals, principalID)
+		}
+	}
+
+	if len(ruleSets) == 0 || !found {
+		ruleSets = append(ruleSets, databricks.RuleSet{
+			Role:       fmt.Sprintf("roles/%s", entitlement.Slug),
+			Principals: []string{principalID},
+		})
+	}
+
+	err = a.client.UpdateRuleSets(ctx, "", "", ruleSets)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to update rule sets for account %s: %w", accID, err)
+	}
+
+	return nil, nil
+}
+
+func (a *accountBuilder) Revoke(ctx context.Context, grant *v2.Grant) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	principal := grant.Principal
+	entitlement := grant.Entitlement
+
+	if !isValidPrincipal(principal.Id) {
+		l.Warn(
+			"databricks-connector: only users and service principals can have account permissions revoked",
+			zap.String("principal_id", principal.Id.String()),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, fmt.Errorf("databricks-connector: only users and service principals can have account permissions revoked")
+	}
+
+	accID := entitlement.Resource.Id.Resource
+	ruleSets, err := a.client.ListRuleSets(ctx, "", "")
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to list rule sets for account %s: %w", accID, err)
+	}
+
+	if len(ruleSets) == 0 {
+		l.Info(
+			"databricks-connector: account already does not have the entitlement",
+			zap.String("principal_id", principal.Id.Resource),
+			zap.String("entitlement", entitlement.Slug),
+		)
+
+		return nil, nil
+	}
+
+	principalID, err := preparePrincipalID(ctx, a.client, principal.Id.ResourceType, principal.Id.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to prepare principal id: %w", err)
+	}
+
+	for i, ruleSet := range ruleSets {
+		if strings.Contains(ruleSet.Role, entitlement.Slug) {
+			// check if it contains the principals and remove the principal to the rule set
+			if slices.Contains(ruleSet.Principals, principalID) {
+				// if there is only one principal, remove the whole rule set
+				if len(ruleSet.Principals) == 1 {
+					ruleSets = slices.Delete(ruleSets, i, i+1)
+					break
+				} else {
+					pI := slices.Index(ruleSet.Principals, principalID)
+					ruleSets[i].Principals = slices.Delete(ruleSet.Principals, pI, pI+1)
+					break
+				}
+			}
+
+			l.Info(
+				"databricks-connector: account already does not have the entitlement",
+				zap.String("principal_id", principalID),
+				zap.String("entitlement", entitlement.Slug),
+			)
+
+			return nil, nil
+		}
+	}
+
+	err = a.client.UpdateRuleSets(ctx, "", "", ruleSets)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to update rule sets for account %s: %w", accID, err)
+	}
+
+	return nil, nil
 }
 
 func newAccountBuilder(client *databricks.Client) *accountBuilder {
