03a6e5f8465b81563ce0217b9fd6f3a4cc0fb95c
Martin Albert <martin.albert.187@gmail.com>
2024-01-24 12:54:58 +0100

    feat: Implement provisioning for service principals

--- pkg/connector/service-principals.go
+++ pkg/connector/service-principals.go
@@ -3,6 +3,7 @@ package connector
 import (
 	"context"
 	"fmt"
+	"slices"
 
 	"github.com/conductorone/baton-databricks/pkg/databricks"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
@@ -11,6 +12,8 @@ import (
 	ent "github.com/conductorone/baton-sdk/pkg/types/entitlement"
 	"github.com/conductorone/baton-sdk/pkg/types/grant"
 	rs "github.com/conductorone/baton-sdk/pkg/types/resource"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -109,17 +112,12 @@ func (s *servicePrincipalBuilder) Entitlements(_ context.Context, resource *v2.R
 
 	groupTrait, err := rs.GetGroupTrait(resource)
 	if err != nil {
-		return nil, "", nil, err
+		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get group trait: %w", err)
 	}
 
-	parentType, ok := rs.GetProfileStringValue(groupTrait.Profile, "parent_type")
-	if !ok {
-		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get parent type from group profile")
-	}
-
-	parentID, ok := rs.GetProfileStringValue(groupTrait.Profile, "parent_id")
-	if !ok {
-		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get parent id from group profile")
+	parentType, parentID, err := getParentInfoFromProfile(groupTrait.Profile)
+	if err != nil {
+		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get parent info from group profile: %w", err)
 	}
 
 	if parentType == workspaceResourceType.Id {
@@ -135,7 +133,7 @@ func (s *servicePrincipalBuilder) Entitlements(_ context.Context, resource *v2.R
 
 	// role permissions entitlements
 	// get all assignable roles for this specific service principal resource
-	roles, err := s.client.ListRoles(context.Background(), "servicePrincipals", applicationId)
+	roles, err := s.client.ListRoles(context.Background(), ServicePrincipalsType, applicationId)
 	if err != nil {
 		return nil, "", nil, fmt.Errorf("databricks-connector: failed to list roles for service principal %s (%s): %w", resource.Id.Resource, applicationId, err)
 	}
@@ -158,17 +156,12 @@ func (s *servicePrincipalBuilder) Entitlements(_ context.Context, resource *v2.R
 func (s *servicePrincipalBuilder) Grants(ctx context.Context, resource *v2.Resource, pToken *pagination.Token) ([]*v2.Grant, string, annotations.Annotations, error) {
 	groupTrait, err := rs.GetGroupTrait(resource)
 	if err != nil {
-		return nil, "", nil, err
+		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get group trait: %w", err)
 	}
 
-	parentType, ok := rs.GetProfileStringValue(groupTrait.Profile, "parent_type")
-	if !ok {
-		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get parent type from group profile")
-	}
-
-	parentID, ok := rs.GetProfileStringValue(groupTrait.Profile, "parent_id")
-	if !ok {
-		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get parent id from group profile")
+	parentType, parentID, err := getParentInfoFromProfile(groupTrait.Profile)
+	if err != nil {
+		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get parent info from group profile: %w", err)
 	}
 
 	if parentType == workspaceResourceType.Id {
@@ -182,7 +175,7 @@ func (s *servicePrincipalBuilder) Grants(ctx context.Context, resource *v2.Resou
 		return nil, "", nil, fmt.Errorf("databricks-connector: failed to get application_id from service principal profile")
 	}
 
-	ruleSets, err := s.client.ListRuleSets(ctx, "servicePrincipals", applicationId)
+	ruleSets, err := s.client.ListRuleSets(ctx, ServicePrincipalsType, applicationId)
 	if err != nil {
 		return nil, "", nil, fmt.Errorf("databricks-connector: failed to list rule sets for service principal %s (%s): %w", resource.Id.Resource, applicationId, err)
 	}
@@ -205,6 +198,177 @@ func (s *servicePrincipalBuilder) Grants(ctx context.Context, resource *v2.Resou
 	}
 
 	return rv, "", nil, nil
+}
+
+func (s *servicePrincipalBuilder) Grant(ctx context.Context, principal *v2.Resource, entitlement *v2.Entitlement) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	if !isValidPrincipal(principal.Id) {
+		l.Warn(
+			"databricks-connector: only users, groups and service principals can be granted service principal permissions",
+			zap.String("principal_id", principal.Id.String()),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, fmt.Errorf("databricks-connector: only users, groups and service principals can be granted service principal permissions")
+	}
+
+	groupTrait, err := rs.GetGroupTrait(entitlement.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to get group trait: %w", err)
+	}
+
+	parentType, parentID, err := getParentInfoFromProfile(groupTrait.Profile)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to get parent info from group profile: %w", err)
+	}
+
+	if parentType == workspaceResourceType.Id {
+		s.client.SetWorkspaceConfig(parentID)
+	} else {
+		s.client.SetAccountConfig()
+	}
+
+	applicationId, ok := rs.GetProfileStringValue(groupTrait.Profile, "application_id")
+	if !ok {
+		return nil, fmt.Errorf("databricks-connector: failed to get application_id from service principal profile")
+	}
+
+	ruleSets, err := s.client.ListRuleSets(ctx, ServicePrincipalsType, applicationId)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to list rule sets for service principal %s (%s): %w", principal.Id.Resource, applicationId, err)
+	}
+
+	principalID, err := preparePrincipalID(ctx, s.client, principal.Id.ResourceType, principal.Id.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to prepare principal id: %w", err)
+	}
+
+	found := false
+
+	for i, ruleSet := range ruleSets {
+		if ruleSet.Role == entitlement.Slug {
+			found = true
+
+			// check if it contains the principals and add principal to the rule set
+			if slices.Contains(ruleSet.Principals, principalID) {
+				l.Info(
+					"databricks-connector: service principal already has the entitlement",
+					zap.String("principal_id", principalID),
+					zap.String("entitlement", entitlement.Slug),
+				)
+
+				return nil, nil
+			}
+
+			// add the principal to the rule set
+			ruleSets[i].Principals = append(ruleSet.Principals, principalID)
+		}
+	}
+
+	if len(ruleSets) == 0 || !found {
+		ruleSets = append(ruleSets, databricks.RuleSet{
+			Role:       entitlement.Slug,
+			Principals: []string{principalID},
+		})
+	}
+
+	err = s.client.UpdateRuleSets(ctx, ServicePrincipalsType, applicationId, ruleSets)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to update rule sets for service principal %s (%s): %w", principal.Id.Resource, applicationId, err)
+	}
+
+	return nil, nil
+}
+
+func (s *servicePrincipalBuilder) Revoke(ctx context.Context, grant *v2.Grant) (annotations.Annotations, error) {
+	l := ctxzap.Extract(ctx)
+
+	principal := grant.Principal
+	entitlement := grant.Entitlement
+
+	if !isValidPrincipal(principal.Id) {
+		l.Warn(
+			"databricks-connector: only users, groups and service principals can have service principal permissions revoked",
+			zap.String("principal_id", principal.Id.String()),
+			zap.String("principal_type", principal.Id.ResourceType),
+		)
+
+		return nil, fmt.Errorf("databricks-connector: only users, groups and service principals can have service principal permissions revoked")
+	}
+
+	groupTrait, err := rs.GetGroupTrait(entitlement.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to get group trait: %w", err)
+	}
+
+	parentType, parentID, err := getParentInfoFromProfile(groupTrait.Profile)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to get parent info from group profile: %w", err)
+	}
+
+	if parentType == workspaceResourceType.Id {
+		s.client.SetWorkspaceConfig(parentID)
+	} else {
+		s.client.SetAccountConfig()
+	}
+
+	applicationId, ok := rs.GetProfileStringValue(groupTrait.Profile, "application_id")
+	if !ok {
+		return nil, fmt.Errorf("databricks-connector: failed to get application_id from service principal profile")
+	}
+
+	ruleSets, err := s.client.ListRuleSets(ctx, ServicePrincipalsType, applicationId)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to list rule sets for service principal %s (%s): %w", principal.Id.Resource, applicationId, err)
+	}
+
+	if len(ruleSets) == 0 {
+		l.Info(
+			"databricks-connector: service principal already does not have the entitlement",
+			zap.String("principal_id", principal.Id.Resource),
+			zap.String("entitlement", entitlement.Slug),
+		)
+
+		return nil, nil
+	}
+
+	principalID, err := preparePrincipalID(ctx, s.client, principal.Id.ResourceType, principal.Id.Resource)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to prepare principal id: %w", err)
+	}
+
+	for i, ruleSet := range ruleSets {
+		if ruleSet.Role == entitlement.Slug {
+			// check if it contains the principals and remove the principal to the rule set
+			if slices.Contains(ruleSet.Principals, principalID) {
+				// if there is only one principal, remove the whole rule set
+				if len(ruleSet.Principals) == 1 {
+					ruleSets = slices.Delete(ruleSets, i, i+1)
+					break
+				} else {
+					pI := slices.Index(ruleSet.Principals, principalID)
+					ruleSets[i].Principals = slices.Delete(ruleSet.Principals, pI, pI+1)
+					break
+				}
+			}
+
+			l.Info(
+				"databricks-connector: service principal already does not have the entitlement",
+				zap.String("principal_id", principalID),
+				zap.String("entitlement", entitlement.Slug),
+			)
+
+			return nil, nil
+		}
+	}
+
+	err = s.client.UpdateRuleSets(ctx, ServicePrincipalsType, applicationId, ruleSets)
+	if err != nil {
+		return nil, fmt.Errorf("databricks-connector: failed to update rule sets for service principal %s (%s): %w", principal.Id.Resource, applicationId, err)
+	}
+
+	return nil, nil
 }
 
 func newServicePrincipalBuilder(client *databricks.Client) *servicePrincipalBuilder {
