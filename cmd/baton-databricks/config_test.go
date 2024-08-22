f16be6a857687a29a63e95e1da7cf6d5a08485e0
Marcos Gaeta <mgaeta89@gmail.com>
2024-08-22 13:27:12 -0700

    use better baton-sdk

--- cmd/baton-databricks/config_test.go
+++ cmd/baton-databricks/config_test.go
@@ -2,84 +2,18 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/conductorone/baton-sdk/pkg/field"
+	"github.com/conductorone/baton-sdk/pkg/test"
+	"github.com/conductorone/baton-sdk/pkg/ustrings"
 	"github.com/spf13/viper"
 )
-
-const (
-	flagSeparator = " "
-	flagPrefix    = "--"
-)
-
-// parseFlags - convert command line flags to a map of keys to strings.
-func parseFlags(expression string) (map[string]string, error) {
-	output := make(map[string]string)
-	tokens := strings.Split(expression, flagSeparator)
-	currentFlag := ""
-	for _, token := range tokens {
-		if strings.HasPrefix(token, flagPrefix) {
-			if currentFlag != "" {
-				output[currentFlag] = ""
-			}
-			currentFlag = strings.TrimPrefix(token, flagPrefix)
-		} else {
-			if currentFlag == "" {
-				return nil, fmt.Errorf("got a value without a flag: %s", token)
-			}
-			output[currentFlag] = token
-			currentFlag = ""
-		}
-	}
-	// Clean up final flag if it exists.
-	if currentFlag != "" {
-		output[currentFlag] = ""
-	}
-
-	return output, nil
-}
-
-func makeViper(input map[string]string) *viper.Viper {
-	output := viper.New()
-	for key, value := range input {
-		output.Set(key, value)
-	}
-	return output
-}
-
-func assertOutcome(
-	t *testing.T,
-	function func() error,
-	expectedSuccess bool,
-) {
-	err := function()
-	if err != nil {
-		if expectedSuccess {
-			t.Fatal("expected function to succeed, but", err.Error())
-		}
-	} else {
-		if !expectedSuccess {
-			t.Fatal("expected function to fail, but it succeeded")
-		}
-	}
-}
 
 func TestConfigs(t *testing.T) {
 	ctx := context.Background()
 
-	configurationSchema := field.NewConfiguration(
-		configurationFields,
-		fieldRelationships...,
-	)
-
-	testCases := []struct {
-		expression string
-		isValid    bool
-		message    string
-	}{
+	testCases := []test.TestCaseFromExpression{
 		{
 			"--account-id 1",
 			false,
@@ -137,26 +71,17 @@ func TestConfigs(t *testing.T) {
 			"not enough tokens",
 		},
 	}
-	for _, testCase := range testCases {
-		t.Run(testCase.message, func(t *testing.T) {
-			values, err := parseFlags(testCase.expression)
-			if err != nil {
-				t.Fatal("could not parse flags:", err)
-			}
 
-			v := makeViper(values)
-
-			assertOutcome(
-				t,
-				func() error {
-					err = field.Validate(configurationSchema, v)
-					if err != nil {
-						return err
-					}
-					return validateConfig(ctx, v)
-				},
-				testCase.isValid,
-			)
-		})
-	}
+	test.ExerciseTestCasesFromExpressions(
+		t,
+		field.NewConfiguration(
+			configurationFields,
+			fieldRelationships...,
+		),
+		func(configs *viper.Viper) error {
+			return validateConfig(ctx, configs)
+		},
+		ustrings.ParseFlags,
+		testCases,
+	)
 }
